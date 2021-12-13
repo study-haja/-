23ff4bc9826d33b7fdff48d3bb0f9f743a26befd
JaeguKim <rlaworn1993@gmail.com>
2021-12-14 00:51:54 +0900

    Add chapter21

--- xUnit/main.py
+++ xUnit/main.py
@@ -6,10 +6,13 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
 
     def tearDown(self):
         pass
@@ -24,8 +27,19 @@ class WasRun(TestCase):
     def testMethod(self):
         self.log= self.log + "testMethod "
 
+    def testBrokenMethod(self):
+        raise Exception
+
     def tearDown(self):
         self.log = self.log + "tearDown "
+
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+    def summary(self):
+        return f"{self.runCount} run, 0 failed"
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
@@ -33,8 +47,21 @@ class TestCaseTest(TestCase):
         self.test.run()
         assert("setUp testMethod tearDown " == self.test.log)
 
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
+
 
 
 if __name__ == '__main__':
     TestCaseTest("testTemplateMethod").run()
+    TestCaseTest("testResult").run()
+    TestCaseTest("testFailedResult").run()
+
 
