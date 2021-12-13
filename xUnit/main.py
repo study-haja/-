38f257a07e59fd1396d5621ef50f22cb54e3d376
JaeguKim <rlaworn1993@gmail.com>
2021-12-14 00:38:01 +0900

    Add chapter20

--- xUnit/main.py
+++ xUnit/main.py
@@ -2,26 +2,39 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
+
+    def tearDown(self):
+        pass
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self, name)
 
+    def setUp(self):
+        self.log = "setUp "
+
     def testMethod(self):
-        self.wasRun = 1
+        self.log= self.log + "testMethod "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 class TestCaseTest(TestCase):
-    def testRunning(self):
-        test= WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+    def testTemplateMethod(self):
+        self.test = WasRun("testMethod")
+        self.test.run()
+        assert("setUp testMethod tearDown " == self.test.log)
+
 
 
 if __name__ == '__main__':
-    TestCaseTest("testRunning").run()
+    TestCaseTest("testTemplateMethod").run()
 
